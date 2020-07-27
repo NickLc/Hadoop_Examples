3487f0053d63fbb412815c71350105b5c824c649
Nick Lázaro Camasca <edsonlc_007@hotmail.com>
2020-07-27 02:26:18 -0700

    Pregunta 5,6,7

---
+++ Pregunta7/SalesCountry/SalesMapper.java
@@ -0,0 +1,19 @@
+package SalesCountry;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+public class SalesMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+
+		String valueString = value.toString();
+		String[] SingleCountryData = valueString.split(",");
+        if(!SingleCountryData[6].equals("State") || !SingleCountryData[3].equals("Payment_Type"))
+		    output.collect(new Text(SingleCountryData[6] + "," + SingleCountryData[3] + ","), new IntWritable( Integer.parseInt(SingleCountryData[2])));
+	}
+}
